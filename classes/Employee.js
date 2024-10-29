ab3221e38a2268b7a0e907385b8a34b814fb35ae
Mohamed Rafeek <mohamed.rafeek@mohamed.rafeek-MacBook-Pro>
2024-10-29 16:58:01 -0400

    added software engineer class, salesperson class and updated employees class

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,34 +1,45 @@
 class Employee {
-    #salary
-    #isHired=true;
-    constructor(name,position,salary) {
-        this.name = name ;
-        this.position=position;
-        this.#salary=salary;
+    #salary;
+    #isHired = true;
+    static #allEmployees = [];
+
+    constructor(name, position, salary) {
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        Employee.#allEmployees.push(this);
     }
-    getSalary(){
+
+    getSalary() {
         return this.#salary;
     }
-    setSalary(amount){
+
+    setSalary(amount) {
         this.#salary = amount;
     }
-    getStatus(){
+
+    getStatus() {
         return this.#isHired;
     }
-    setStatus(command){
-        if(command === "hire"){
-            this.#isHired=true;
-        }else if(command === "fire"){
-            this.#isHired=false ;
 
+    setStatus(command) {
+        if (command === "hire") {
+            this.#isHired = true;
+        } else if (command === "fire") {
+            this.#isHired = false;
         }
-
     }
 
+    static getEmployees() {
+        return Employee.#allEmployees;
+    }
 
-
-    
+    // Static method to calculate the total payroll
+    static getTotalPayroll() {
+        return Employee.#allEmployees.reduce((total, employee) => total + employee.getSalary(), 0);
+    }
 }
+
 
 module.exports = {
     Employee,
